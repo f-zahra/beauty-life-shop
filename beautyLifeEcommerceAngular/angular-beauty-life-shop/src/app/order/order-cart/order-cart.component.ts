5916a121468fe3f40434570526bf076887762cc9
f-zahra <eva-200@hotmail.fr>
2024-04-11 15:03:06 -0400

    created order-cart component

---
+++ beautyLifeEcommerceAngular/angular-beauty-life-shop/src/app/order/order-cart/order-cart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-cart',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-cart.component.html',
+  styleUrl: './order-cart.component.css'
+})
+export class OrderCartComponent {
+
+}
