25ba90d3194cc3156d02dffdc48b9da237bf1959
f-zahra <eva-200@hotmail.fr>
2024-04-12 15:37:10 -0400

    update checkout UI

--- beautyLifeEcommerceAngular/angular-beauty-life-shop/src/app/order/order-cart/order-cart.component.ts
+++ beautyLifeEcommerceAngular/angular-beauty-life-shop/src/app/order/order-cart/order-cart.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ShoppingCart } from '../../types';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-order-cart',
   standalone: true,
   imports: [],
   templateUrl: './order-cart.component.html',
-  styleUrl: './order-cart.component.css'
+  styleUrl: './order-cart.component.css',
 })
 export class OrderCartComponent {
+  cart!: ShoppingCart;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.getShoppingCart().subscribe((data: ShoppingCart) => {
+      this.cart = data;
+      console.log(this.cart);
+    });
+  }
 }
--- beautyLifeEcommerceAngular/angular-beauty-life-shop/src/app/order/order.component.ts
+++ beautyLifeEcommerceAngular/angular-beauty-life-shop/src/app/order/order.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { OrderCartComponent } from './order-cart/order-cart.component';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [OrderCartComponent],
+  imports: [OrderCartComponent, FormsModule],
   templateUrl: './order.component.html',
   styleUrl: './order.component.css',
 })
-export class OrderComponent {}
+export class OrderComponent {
+  isFormDisabled: boolean = false;
+
+  toggle() {
+    this.isFormDisabled = !this.isFormDisabled;
+  }
+
+  onSubmit(formData: any) {
+    console.log(formData); // Access the form data here
+  }
+}
